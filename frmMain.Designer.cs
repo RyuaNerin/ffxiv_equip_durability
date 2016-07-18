752051671747c5f33c54bbd1766923126b56a522
RyuaNerin <admin@ryuanerin.kr>
2016-07-19 05:34:49 +0900

    first commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ffxiv_equip_durability
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDrag = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDrag
+            // 
+            this.lblDrag.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDrag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblDrag.Location = new System.Drawing.Point(0, 0);
+            this.lblDrag.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDrag.Name = "lblDrag";
+            this.lblDrag.Size = new System.Drawing.Size(6, 100);
+            this.lblDrag.TabIndex = 1;
+            this.lblDrag.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblDrag_MouseDown);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(259, 100);
+            this.Controls.Add(this.lblDrag);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.Text = "eq_dura";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDrag;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ffxiv_equip_durability
+{
+    public partial class frmMain : Form
+    {
+        private static class NativeMethods
+        {
+            public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+            [DllImport("user32.dll")]
+            public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+            
+            [DllImport("user32.dll")]
+            public static extern bool ReleaseCapture();
+
+            [DllImport("user32.dll")]
+            public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int processId);
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+            [DllImport("user32.dll")]
+            public static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+            [DllImport("user32.dll", SetLastError=true)]
+            public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+            [DllImport("user32.dll")]
+            public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+            
+            [DllImport("user32.dll")]
+            public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+            [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool IsWow64Process([In] IntPtr process, [Out] out bool wow64Process);
+            
+		    [DllImport("kernel32.dll")]
+		    public static extern IntPtr OpenProcess(uint dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+		    [DllImport("kernel32.dll")]
+		    public static extern Int32 CloseHandle(IntPtr hProcess);
+
+		    [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+
+            public const int WS_EX_LAYERED = 0x80000;
+            public const int WS_EX_TOOLWINDOW = 0x80;
+            public const int WM_NCLBUTTONDOWN = 0xA1;
+            public const int HT_CAPTION = 0x2;
+
+            public const int EVENT_SYSTEM_MENUEND = 0x5;
+            public const int EVENT_SYSTEM_FOREGROUND = 0x3;
+
+            public const int WINEVENT_OUTOFCONTEXT = 0;
+            public const int WINEVENT_SKIPOWNPROCESS = 2;
+            
+            public static bool IsX64(Process process)
+            {
+                var version = Environment.OSVersion.Version;
+                if ((version.Major > 5) || ((version.Major == 5) && (version.Minor >= 1)))
+                {
+                    bool retVal;
+                    return !(NativeMethods.IsWow64Process(process.Handle, out retVal) && retVal);
+                }
+
+                return false; // not on 64-bit Windows Emulator
+            }
+
+            public static byte[] ReadMemory(byte[] buff, IntPtr hproc, IntPtr addr, int numOfBytes)
+            {
+                int bytesRead;
+                NativeMethods.ReadProcessMemory(hproc, addr, buff, numOfBytes, out bytesRead);
+
+                return buff;
+            }
+        }
+
+        readonly NativeMethods.WinEventDelegate m_hookProc;
+        readonly Label[] m_dura = new Label[13];
+        readonly Label[] m_spir = new Label[13];
+        Process m_proc;
+        bool m_isX64;
+        IntPtr m_hproc;
+        IntPtr m_hhook;
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            
+            this.m_hookProc = new NativeMethods.WinEventDelegate(this.WinEventProc);
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            NativeMethods.UnhookWinEvent(this.m_hhook);
+            NativeMethods.CloseHandle(this.m_hproc);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+//             int initialStyle = NativeMethods.GetWindowLong(this.Handle, -20);
+//             NativeMethods.SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+
+            this.Width = 90;
+
+            int y = 2;
+            for (int i = 0; i < m_dura.Length; ++i)
+            {
+                this.m_spir[i] = new Label
+                {
+                    AutoSize = false,
+                    Left = 8,
+                    Top = y,
+                    Width = 40,
+                    Height = 10,
+                    Text = "0.0",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                };
+
+                this.m_dura[i] = new Label
+                {
+                    AutoSize = false,
+                    Left = 50,
+                    Top = y,
+                    Width = 40,
+                    Height = 10,
+                    Text = "0.0 %",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                };
+                
+                if (i != this.m_dura.Length)
+                    y += 11;
+
+                if (i == 1)
+                    y += 6;
+                else if (i == 7)
+                    y += 6;
+
+
+                this.Controls.Add(this.m_dura[i]);
+                this.Controls.Add(this.m_spir[i]);
+            }
+
+            this.Height = y + 2;
+
+            Task.Factory.StartNew(this.Worker);
+
+        }
+
+        private void lblDrag_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                NativeMethods.ReleaseCapture();
+                NativeMethods.SendMessage(this.Handle, NativeMethods.WM_NCLBUTTONDOWN, NativeMethods.HT_CAPTION, 0);
+            }
+        }
+        
+        private void Worker()
+        {
+            int pid;
+            IntPtr ptr;
+            byte[] buff = new byte[0x40 * 13];
+            int i;
+            int offset;
+
+            while (true)
+            {
+                if ((this.m_proc == null) || this.m_proc.HasExited)
+                {
+                    NativeMethods.UnhookWinEvent(this.m_hhook);
+                    NativeMethods.CloseHandle(this.m_hproc);
+                    this.m_proc = null;
+
+                    ptr = NativeMethods.FindWindow("FFXIVGAME", null);
+                    if (ptr != IntPtr.Zero)
+                    {
+                        if (NativeMethods.GetWindowThreadProcessId(ptr, out pid) != 0)
+                        {
+                            this.m_proc = Process.GetProcessById(pid);
+                            if (this.m_proc != null)
+                            {
+                                this.m_hproc = NativeMethods.OpenProcess(0x00000010, false, this.m_proc.Id);
+                                if (this.m_hproc != IntPtr.Zero)
+                                {
+                                    this.Invoke(new Action(
+                                        () => this.m_hhook = NativeMethods.SetWinEventHook(
+                                            NativeMethods.EVENT_SYSTEM_FOREGROUND,
+                                            NativeMethods.EVENT_SYSTEM_FOREGROUND,
+                                            IntPtr.Zero,
+                                            this.m_hookProc,
+                                            0,
+                                            0,
+                                            NativeMethods.WINEVENT_OUTOFCONTEXT)
+                                        ));
+
+                                    this.m_isX64 = NativeMethods.IsX64(this.m_proc);
+
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+
+                    this.m_proc = null;
+
+                    Thread.Sleep(10 * 1000);
+                }
+                else
+                {
+                    ptr = m_proc.MainModule.BaseAddress + (this.m_isX64 ? 0x0163E690 : 0x100CDB8);
+                    ptr = new IntPtr(BitConverter.ToInt32(NativeMethods.ReadMemory(buff, this.m_hproc, ptr, 4), 0)) + 0x60;
+                    ptr = new IntPtr(BitConverter.ToInt32(NativeMethods.ReadMemory(buff, this.m_hproc, ptr, 4), 0));
+                    
+                    NativeMethods.ReadMemory(buff, this.m_hproc, ptr, 0x40 * 13);
+
+                    for (i = 0; i < 13; ++i)
+                    {
+                        offset = i * 0x40;
+                        if (BitConverter.ToUInt16(buff, offset + 0x08) == 0)
+                            this.SetValue(i, -1, -1);
+                        else
+                            this.SetValue(i, BitConverter.ToUInt16(buff, offset + 0x10), BitConverter.ToUInt16(buff, offset + 0x12));
+                    }
+                    
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            if (idObject != 0 || idChild != 0)
+                return;
+
+            NativeMethods.SetWindowPos(this.Handle, new IntPtr(/*HWND_TOPMOST*/-1), 0, 0, 0, 0, /*SWP_NOMOVE | SWP_NOSIZE*/ 3);
+        }
+
+        private void SetValue(int index, int spir, int dura)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new Action<int, int, int>(this.SetValue), index, spir, dura);
+            else
+            {
+                this.m_spir[index].Text = (spir == -1) ? "-" : string.Format("{0:##0.0} %", spir / 100d);
+                this.m_dura[index].Text = (dura == -1) ? "-" : string.Format("{0:##0.0} %", dura / 300d);
+            }
+        }
+    }
+}
